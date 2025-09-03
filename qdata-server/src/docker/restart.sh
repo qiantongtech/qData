f0855876a0bf539387d3c2a21c73c8546047469e
caoyun <1942152752@qq.com>
2025-09-03 12:44:05 +0800

    build(docker): 更新 Docker镜像构建配置
    - 更新 .gitignore 文件，添加新的 Docker相关文件和目录
    - 删除旧的 Dockerfile 和 docker-compose.yml 文件
    - 新增 .env 和 .env.example 文件，包含环境变量配置
    - 新增 application-prod.yml 文件，配置生产环境参数- 新增 build.sh 文件，实现 Docker 镜像构建和启动
    - 新增 definitions.json 文件，配置 RabbitMQ 队列和交换机
    - 新增 dependency-reduced-pom.xml 文件，配置 ETL 项目的 Maven 构建

--- qdata-server/src/docker/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-path="/opt/qdata"
-image="qdata"
-
-cd ${path}
-docker-compose stop ${image}  &&  docker-compose rm -f ${image} && docker rmi ${image}
-docker build -t ${image}:latest .
-docker-compose create ${image}  && docker-compose start ${image}

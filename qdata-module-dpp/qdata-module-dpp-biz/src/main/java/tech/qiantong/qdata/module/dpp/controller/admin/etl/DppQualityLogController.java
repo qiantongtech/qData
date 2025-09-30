ef88e91c70584e370c343916c5e1ce9d36949573
mengfanming <mengfanming@qiantong.tech>
2025-09-30 15:38:38 +0800

    数据质量查询日志查询bug

--- qdata-module-dpp/qdata-module-dpp-biz/src/main/java/tech/qiantong/qdata/module/dpp/controller/admin/etl/DppQualityLogController.java
+++ qdata-module-dpp/qdata-module-dpp-biz/src/main/java/tech/qiantong/qdata/module/dpp/controller/admin/etl/DppQualityLogController.java
@@ -3,6 +3,9 @@ package tech.qiantong.qdata.module.dpp.controller.admin.etl;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
 import cn.hutool.core.date.DateUtil;
 import java.util.List;
@@ -13,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import tech.qiantong.qdata.common.core.domain.AjaxResult;
+import tech.qiantong.qdata.common.core.domain.ReturnT;
 import tech.qiantong.qdata.common.core.page.PageParam;
 import tech.qiantong.qdata.common.annotation.Log;
 import tech.qiantong.qdata.common.core.controller.BaseController;
@@ -24,6 +28,7 @@ import tech.qiantong.qdata.common.utils.poi.ExcelUtil;
 import tech.qiantong.qdata.module.dpp.controller.admin.etl.vo.DppQualityLogPageReqVO;
 import tech.qiantong.qdata.module.dpp.controller.admin.etl.vo.DppQualityLogRespVO;
 import tech.qiantong.qdata.module.dpp.controller.admin.etl.vo.DppQualityLogSaveReqVO;
+import tech.qiantong.qdata.module.dpp.controller.admin.etl.vo.LogResult;
 import tech.qiantong.qdata.module.dpp.convert.etl.DppQualityLogConvert;
 import tech.qiantong.qdata.module.dpp.dal.dataobject.etl.DppQualityLogDO;
 import tech.qiantong.qdata.module.dpp.service.etl.IDppQualityLogService;
@@ -102,6 +107,37 @@ public class DppQualityLogController extends BaseController {
     @DeleteMapping("/{ids}")
     public CommonResult<Integer> remove(@PathVariable Long[] ids) {
         return CommonResult.toAjax(dppQualityLogService.removeDppQualityLog(Arrays.asList(ids)));
+    }
+
+
+    @RequestMapping(value = "/logDetailCat", method = RequestMethod.GET)
+    @Operation(summary = "运行日志详情")
+    public ReturnT<LogResult> logDetailCat(String handleMsg) {
+        // 添加日志审计功能
+        try {
+            InputStream in = new FileInputStream(handleMsg);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) != -1) {
+                bos.write(buf, 0, len);
+            }
+            String logContent = new String(bos.toByteArray(), "UTF-8");
+            if (bos != null) {
+                bos.close();
+            }
+            if (in != null) {
+                in.close();
+            }
+            // @TODO 查看日志
+            ReturnT<LogResult> returnT = new ReturnT<>(ReturnT.SUCCESS_CODE, "查询日志成功");
+            LogResult logResult = new LogResult(0, 0, logContent, true);
+            returnT.setContent(logResult);
+            return returnT;
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return new ReturnT<>(ReturnT.FAIL_CODE, "暂未找到日志文件信息");
+        }
     }
 
 }

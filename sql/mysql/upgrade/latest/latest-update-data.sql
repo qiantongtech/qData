a01fce63a0570ef319c70437977d9fd2ea7c98d9
zhangguobin <zhangguobinw@qq.com>
2025-12-12 17:52:16 +0800

    feat(etl): 清洗规则新增规则：清洗过期记录；字段值转大写；超长字段截取；小数位统一；正则表达式替换。sql脚本

---
+++ sql/mysql/upgrade/latest/latest-update-data.sql
@@ -0,0 +1,5 @@
+UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='008';
+UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='011';
+UPDATE qdata.att_clean_rule	SET valid_flag='1', strategy_key='STRING_SUBSTR' WHERE code='012';
+UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='021';
+UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='039';

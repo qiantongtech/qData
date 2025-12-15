c4a8004cb7a8f691e0932e94b48ca8731a59a074
zhangguobin <zhangguobinw@qq.com>
2025-12-15 11:02:56 +0800

    feat(etl): 数据稽查规则,数值范围校验；字段长度范围校验；枚举校验。sql脚本

--- sql/mysql/upgrade/latest/latest-update-data.sql
+++ sql/mysql/upgrade/latest/latest-update-data.sql
@@ -3,3 +3,5 @@ UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='011';
 UPDATE qdata.att_clean_rule	SET valid_flag='1', strategy_key='STRING_SUBSTR' WHERE code='012';
 UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='021';
 UPDATE qdata.att_clean_rule	SET valid_flag='1' WHERE code='039';
+
+UPDATE qdata.att_audit_rule SET valid_flag='1' WHERE code in('306', '108', '111');

087377913c31bf4ccb3b4b72bafbed3157c05c10
FXB <1072651764@qq.com>
2025-12-24 16:31:26 +0800

    feat(task): - etl参数json中添加redis的维护 - 数据集成增量同步的重构

--- qdata-ui/src/views/dpp/task/integratioTask/components/input/inputColumnEdit.vue
+++ qdata-ui/src/views/dpp/task/integratioTask/components/input/inputColumnEdit.vue
@@ -33,37 +33,38 @@
 <template>
   <el-dialog v-model="visibleDialog" draggable class="dialog" :title="title" destroy-on-close>
     <el-form ref="daDiscoveryTaskRef" :model="form" label-width="120px" @submit.prevent>
+
       <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="字段名称" prop="incrementColumn" :rules="[
-            { required: true, message: '请输入字段名称', trigger: 'blur' },
+        <el-col :span="24">
+          <el-form-item label="基准类型" prop="type" :rules="[
+            { required: true, message: '请选择基准类型', trigger: 'change' },
           ]">
-            <el-select v-model="form.incrementColumn" placeholder="请输入字段名称">
-              <el-option v-for="item in ColumnByAssettab" :key="item.columnName" :label="item.columnName"
-                :value="item.columnName" />
-            </el-select>
-          </el-form-item>
-        </el-col>
-        <el-col :span="12">
-          <el-form-item label="运算符" prop="operator" :rules="[
-            { required: true, message: '请选择运算符', trigger: 'change' },
-          ]">
-            <el-select v-model="form.operator" placeholder="请选择运算符">
-              <el-option v-for="operator in operators" :key="operator.value" :label="operator.label"
-                :value="operator.value" />
+            <el-select v-model="form.type" placeholder="请选择基准类型">
+              <el-option v-for="benchmark in benchmarkTypes" :key="benchmark.value" :label="benchmark.label"
+                         :value="benchmark.value" />
             </el-select>
           </el-form-item>
         </el-col>
       </el-row>
 
       <el-row :gutter="20">
-        <el-col :span="12">
-          <el-form-item label="基准类型" prop="type" :rules="[
-            { required: true, message: '请选择基准类型', trigger: 'change' },
+        <el-col :span="24">
+          <el-form-item label="字段名称" prop="incrementColumn" :rules="[
+            { required: true, message: '请输入字段名称', trigger: 'blur' },
           ]">
-            <el-select v-model="form.type" placeholder="请选择基准类型">
-              <el-option v-for="benchmark in benchmarkTypes" :key="benchmark.value" :label="benchmark.label"
-                :value="benchmark.value" />
+            <el-select v-model="form.incrementColumn" placeholder="请输入字段名称">
+              <el-option v-for="item in ColumnByAssettab" :key="item.columnName" :label="item.columnName"
+                         :value="item.columnName" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="24" v-if="form.type !== '2'">
+          <el-form-item label="运算符" prop="operator" :rules="[
+            { required: true, message: '请选择运算符', trigger: 'change' },
+          ]">
+            <el-select v-model="form.operator" placeholder="请选择运算符">
+              <el-option v-for="operator in operators" :key="operator.value" :label="operator.label"
+                         :value="operator.value" />
             </el-select>
           </el-form-item>
         </el-col>
@@ -83,11 +84,23 @@
             </template>
             <template v-else-if="form.type === '3'">
               <sql-editor placeholder="请输入sql" ref="editorRef" :value="form.data" class="sql-editor" :height="'300px'"
-                @changeTextarea="changeTextarea($event)" />
+                          @changeTextarea="changeTextarea($event)" />
             </template>
             <template v-else>
               <el-input v-model="form.data" placeholder="自动获取当前时间，无需填写" disabled />
             </template>
+          </el-form-item>
+        </el-col>
+
+        <el-col :span="24" v-if="form.type === '2'">
+          <el-form-item label="游标时间" prop="cursorTime" :rules="[{ required: true, message: '请选择游标时间', trigger: 'change' }]">
+            <el-date-picker clearable
+                            v-model="form.cursorTime"
+                            :type="pickerType"
+                            :format="dateIncrementConfig_dateFormat2"
+                            :value-format="dateIncrementConfig_dateFormat2"
+                            placeholder="请选择游标时间">
+            </el-date-picker>
           </el-form-item>
         </el-col>
       </el-row>
@@ -149,7 +162,7 @@ const operators = ref([
 // 基准类型
 const benchmarkTypes = ref([
   { label: "固定值", value: "1" },
-  { label: "自动(当前时间)", value: "2" },
+  { label: "时间范围", value: "2" },
   { label: "SQL表达式", value: "3" },
 ]);
 
@@ -163,25 +176,25 @@ const dataRules = computed(() => {
 
 // 监听 visible 弹窗打开时初始化表单
 watch(
-  () => props.visible,
-  (newVal) => {
-    if (newVal) {
-      form.value = JSON.parse(JSON.stringify(props.data || {}));
-    } else {
-      proxy.resetForm("daDiscoveryTaskRef");
+    () => props.visible,
+    (newVal) => {
+      if (newVal) {
+        form.value = JSON.parse(JSON.stringify(props.data || {}));
+      } else {
+        proxy.resetForm("daDiscoveryTaskRef");
+      }
     }
-  }
 );
 
 // 默认选择字段名称
 watch(
-  () => props.ColumnByAssettab,
-  (newVal) => {
-    if (newVal?.length > 0 && !form.value.incrementColumn) {
-      form.value.incrementColumn = newVal[0].columnName;
-    }
-  },
-  { immediate: true, deep: true }
+    () => props.ColumnByAssettab,
+    (newVal) => {
+      if (newVal?.length > 0 && !form.value.incrementColumn) {
+        form.value.incrementColumn = newVal[0].columnName;
+      }
+    },
+    { immediate: true, deep: true }
 );
 
 // 监听类型变化重置 data

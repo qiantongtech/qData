1c5cf323dd63b5dd1cee4ef32b358a2a3cc3ddb5
renmengting <renmengting@qiantong.tech>
2026-01-12 14:04:42 +0800

    feat: 新增Kafka输入组件及多个转换组件功能 新增Kafka输入组件及相关表单页面，实现Kafka数据源配置和字段映射功能 新增去除重复记录、增加常量、字段选择修改、值映射四个转换组件 为Kafka输入组件添加JSON解析值长度限制和提示 优化表单校验逻辑和用户体验

---
+++ qdata-ui/src/views/dpp/task/integratioTask/components/input/kafkaForm.vue
@@ -0,0 +1,519 @@
+<!--
+  Copyright © 2025 Qiantong Technology Co., Ltd.
+  qData Data Middle Platform (Open Source Edition)
+   *
+  License:
+  Released under the Apache License, Version 2.0.
+  You may use, modify, and distribute this software for commercial purposes
+  under the terms of the License.
+   *
+  Special Notice:
+  All derivative versions are strictly prohibited from modifying or removing
+  the default system logo and copyright information.
+  For brand customization, please apply for brand customization authorization via official channels.
+   *
+  More information: https://qdata.qiantong.tech/business.html
+   *
+  ============================================================================
+   *
+  版权所有 © 2025 江苏千桐科技有限公司
+  qData 数据中台（开源版）
+   *
+  许可协议：
+  本项目基于 Apache License 2.0 开源协议发布，
+  允许在遵守协议的前提下进行商用、修改和分发。
+   *
+  特别说明：
+  所有衍生版本不得修改或移除系统默认的 LOGO 和版权信息；
+  如需定制品牌，请通过官方渠道申请品牌定制授权。
+   *
+  更多信息请访问：https://qdata.qiantong.tech/business.html
+-->
+
+<template>
+  <el-dialog
+    v-model="visibleDialog"
+    :draggable="true"
+    :title="form.taskParams.typeName"
+    showCancelButton
+    :show-close="false"
+    class="medium-dialog"
+    destroy-on-close
+  >
+    <el-form
+      ref="dpModelRefs"
+      :model="form"
+      label-width="110px"
+      @submit.prevent
+      v-loading="loading"
+      :rules="rules"
+    >
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item
+            label="节点名称"
+            prop="name"
+            :rules="[
+              { required: true, message: '请输入节点名称', trigger: 'change' },
+            ]"
+          >
+            <el-input
+              v-if="!info"
+              v-model="form.name"
+              placeholder="请输入节点名称"
+            />
+            <div v-else class="form-readonly">{{ form.name }}</div>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="类型" prop="typeName">
+            <el-select
+              v-if="!info"
+              v-model="form.taskParams.typeName"
+              placeholder="请输入类型"
+              filterable
+              disabled
+            >
+              <el-option
+                v-for="dict in typeList"
+                :key="dict.value"
+                :label="dict.label"
+                :value="dict.value"
+              ></el-option>
+            </el-select>
+            <div v-else class="form-readonly">
+              {{ form.taskParams.typeName }}
+            </div>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="24">
+          <el-form-item label="描述" prop="description">
+            <el-input
+              v-if="!info"
+              v-model="form.description"
+              type="textarea"
+              maxlength="500个字符"
+              show-word-limit
+              placeholder="请输入描述"
+            />
+            <div v-else class="form-readonly textarea">
+              {{ form.description || "-" }}
+            </div>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-form-item
+            label="数据源连接"
+            prop="taskParams.readerDatasource.datasourceId"
+            :rules="[
+              {
+                required: true,
+                message: '请选择源数据库连接',
+                trigger: 'change',
+              },
+            ]"
+          >
+            <el-select
+              v-if="!info"
+              v-model="form.taskParams.readerDatasource.datasourceId"
+              placeholder="请选择源数据库连接"
+              @change="handleDatasourceChange"
+              filterable
+            >
+              <el-option
+                v-for="dict in createTypeList"
+                :key="dict.id"
+                :label="dict.datasourceName"
+                :value="dict.id"
+              ></el-option>
+            </el-select>
+            <div v-else class="form-readonly">
+              {{ form.taskParams.readerDatasource?.datasourceId || "-" }}
+            </div>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item
+            label="主题名称"
+            prop="taskParams.topic"
+            :rules="[
+              {
+                required: true,
+                message: '请输入主题名称',
+                trigger: 'change',
+              },
+            ]"
+          >
+            <el-input
+              v-if="!info"
+              v-model="form.taskParams.topic"
+              placeholder="请输入主题名称"
+            />
+            <div v-else class="form-readonly">
+              {{ form.taskParams.topic || "-" }}
+            </div>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="24">
+          <el-form-item label="配置参数" prop="taskParams.config">
+            <el-input
+              v-if="!info"
+              v-model="form.taskParams.config"
+              type="textarea"
+              maxlength="500个字符"
+              show-word-limit
+              placeholder='例如: {"group.id"&colon;"demo_test"}'
+            />
+            <div v-else class="form-readonly textarea">
+              {{ form.taskParams.config || "-" }}
+            </div>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-divider content-position="center">
+        <span class="blue-text">属性字段</span>
+      </el-divider>
+      <div class="justify-between mb15" v-if="!info">
+        <el-row :gutter="15" class="btn-style">
+          <el-col :span="1.5">
+            <el-button type="primary" plain @click="openDialog()">
+              <i class="iconfont-mini icon-xinzeng mr5"></i>新增
+            </el-button>
+          </el-col>
+        </el-row>
+      </div>
+      <el-table
+        stripe
+        height="240px"
+        v-loading="loadingList"
+        :data="ColumnByAssettab"
+      >
+        <el-table-column label="序号" type="index" width="80" align="left">
+          <template #default="scope">
+            <span>{{ scope.$index + 1 }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="字段名称"
+          align="left"
+          prop="columnName"
+          :show-overflow-tooltip="{ effect: 'light' }"
+        >
+          <template #default="scope">
+            {{ scope.row.columnName || "-" }}
+          </template>
+        </el-table-column>
+        <el-table-column label="字段类型" align="left" prop="columnType">
+          <template #default="scope">
+            {{ scope.row.columnType || "-" }}
+          </template>
+        </el-table-column>
+        <el-table-column label="JSON解析取值" align="left" prop="key">
+          <template #default="scope">
+            {{ scope.row.key || "-" }}
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-if="!info"
+          label="操作"
+          align="center"
+          class-name="small-padding fixed-width"
+          fixed="right"
+          width="240"
+        >
+          <template #default="scope">
+            <el-button
+              link
+              type="primary"
+              icon="Edit"
+              @click="openDialog({ ...scope.row, index: scope.$index + 1 })"
+              >修改</el-button
+            >
+            <el-button
+              type="danger"
+              link
+              icon="Delete"
+              :disabled="multiple"
+              @click="handleDelete(scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-form>
+    <template #footer>
+      <div style="text-align: right">
+        <el-button @click="closeDialog">关闭</el-button>
+        <el-button type="primary" @click="saveData" v-if="!info"
+          >保存</el-button
+        >
+      </div>
+    </template>
+  </el-dialog>
+  <kafkaUploadDialog
+    :visible="open"
+    title="属性字段编辑"
+    @update:visible="open = $event"
+    @confirm="handletaskConfig"
+    :data="row"
+  />
+</template>
+<script setup>
+import {
+  getTablesByDataSourceId,
+  getNodeUniqueKey,
+} from "@/api/dpp/task/index.js";
+import kafkaUploadDialog from "../kafkaUpload.vue";
+import { listDaDatasourceByProjectCode } from "@/api/da/dataSource/dataSource.js";
+const { proxy } = getCurrentInstance();
+import useUserStore from "@/store/system/user.js";
+const userStore = useUserStore();
+const props = defineProps({
+  visible: { type: Boolean, default: true },
+  title: { type: String, default: "表单标题" },
+  currentNode: { type: Object, default: () => ({}) },
+  info: { type: Boolean, default: false },
+});
+const emit = defineEmits(["update", "confirm"]);
+const visibleDialog = computed({
+  get() {
+    return props.visible;
+  },
+  set(newValue) {
+    emit("update", newValue);
+  },
+});
+let rules = {
+  config: [
+    {
+      trigger: "blur",
+      validator: (rule, value, callback) => {
+        if (value === null || value === undefined || value === "") {
+          callback();
+          return;
+        }
+        var flag = false;
+        if (typeof value === "string") {
+          try {
+            const obj = JSON.parse(value);
+            if (typeof obj === "object" && obj) {
+              flag = true;
+            }
+          } catch (e) {}
+        }
+        if (flag) {
+          callback();
+        } else {
+          callback("不是一个正确的JSON格式");
+        }
+      },
+    },
+  ],
+};
+// 变量定义
+let loading = ref(false);
+let loadingList = ref(false);
+let TablesByDataSource = ref([]);
+let ColumnByAssettab = ref([]);
+// 修改
+const open = ref(false);
+let row = ref({});
+const openDialog = (obj) => {
+  row.value = obj;
+  open.value = true;
+};
+const handletaskConfig = (obj) => {
+  if (row.value?.index) {
+    ColumnByAssettab.value[row.value.index - 1] = {
+      ...ColumnByAssettab.value[row.value.index - 1],
+      ...obj,
+    };
+  } else {
+    ColumnByAssettab.value.push({ ...obj });
+  }
+};
+// 删除
+const handleDelete = (row) => {
+  if (!row?.columnName) {
+    console.warn("删除失败：row 没有 columnName");
+    return;
+  }
+  ColumnByAssettab.value = ColumnByAssettab.value.filter(
+    (item) => item.columnName !== row.columnName
+  );
+};
+let dpModelRefs = ref();
+let form = ref({});
+const tableFields = ref([]); // 来源表格
+const createTypeList = ref([]); // 数据源列表
+// 获取数据源列表
+const getDatasourceList = async () => {
+  try {
+    loading.value = true;
+    const response = await listDaDatasourceByProjectCode({
+      projectCode: userStore.projectCode,
+      projectId: userStore.projectId,
+      datasourceType: "Kafka",
+    });
+    createTypeList.value = response.data.rows;
+  } finally {
+    loading.value = false;
+  }
+};
+// 获取表列表
+const getTablesByDatasourceId = async (id) => {
+  TablesByDataSource.value = await fetchData(
+    getTablesByDataSourceId,
+    { datasourceId: id },
+    loadingList
+  );
+};
+// 通用的获取数据的函数
+const fetchData = async (requestFn, params, loadingState) => {
+  try {
+    loadingState.value = true;
+    const response = await requestFn(params);
+    return response.data;
+  } finally {
+    loadingState.value = false;
+  }
+};
+
+// 处理数据源变化
+const resetAndFetchTables = async (selectedDatasource) => {
+  TablesByDataSource.value = [];
+  let { datasourceType, datasourceConfig, ip, port, id, config } =
+    selectedDatasource;
+  let code = JSON.parse(datasourceConfig);
+  form.value.taskParams.config = config;
+  form.value.taskParams.asset_id = "";
+  form.value.taskParams.readerDatasource = {
+    datasourceType,
+    datasourceConfig,
+    ip,
+    port,
+    dbname: code.dbname,
+    datasource_id: id,
+    datasourceId: id,
+  };
+
+  // await getTablesByDatasourceId(id);
+};
+
+// 处理数据源变化
+const handleDatasourceChange = (value) => {
+  const selectedDatasource = createTypeList.value.find(
+    (item) => item.id == value
+  );
+  if (selectedDatasource) {
+    resetAndFetchTables(selectedDatasource);
+  }
+};
+
+const off = () => {
+  proxy.resetForm("dpModelRefs");
+  TablesByDataSource.value = [];
+  tableFields.value = [];
+};
+// 保存数据
+const saveData = async () => {
+  try {
+    // 异步验证表单
+    const valid = await dpModelRefs.value.validate();
+    if (!valid) return;
+    if (
+      form.value?.taskParams.type == "1" &&
+      (!ColumnByAssettab.value || ColumnByAssettab.value.length == 0)
+    ) {
+      return proxy.$message.warning("校验未通过，请选择属性字段");
+    }
+    // 如果没有 code，就调用接口获取唯一的 code
+    if (!form.value.code) {
+      loading.value = true;
+      const response = await getNodeUniqueKey({
+        projectCode: userStore.projectCode,
+        projectId: userStore.projectId,
+      });
+      loading.value = false;
+      form.value.code = response.data;
+    }
+    const taskParams = form.value?.taskParams;
+    (taskParams.tableFields = ColumnByAssettab.value.map((item) => {
+      return {
+        ...item,
+        code: form.value.code,
+      };
+    })),
+      (taskParams.columnsList = ColumnByAssettab.value.map(
+        ({ columnName, columnType }) => ({
+          colName: columnName,
+          dataType: columnType,
+        })
+      ));
+    taskParams.columns = taskParams.tableFields.map((item) => {
+      return {
+        name: item.columnName,
+        type: item.columnType,
+        key: item.key,
+      };
+    });
+    emit("confirm", form.value);
+  } catch (error) {
+    loading.value = false;
+  }
+};
+const closeDialog = () => {
+  off();
+  // 关闭对话框
+  emit("update", false);
+};
+
+// 监听属性变化
+function deepCopy(data) {
+  if (data === undefined || data === null) {
+    return {}; // 或者返回一个默认值
+  }
+  try {
+    return JSON.parse(JSON.stringify(data));
+  } catch (e) {
+    return {}; // 或者返回一个默认值
+  }
+}
+// 处理数据源和列操作的共用函数
+const handleDatasource = (datasource, assetId) => {
+  if (datasource?.datasourceId) {
+    // getTablesByDatasourceId(datasource.datasourceId);
+  } else {
+    console.warn("无效的数据源信息", datasource);
+  }
+};
+// 监听属性变化
+watchEffect(() => {
+  if (props.visible) {
+    getDatasourceList();
+
+    // 数据源
+    form.value = deepCopy(props.currentNode.data);
+    ColumnByAssettab.value =
+      deepCopy(props.currentNode.data.taskParams.tableFields) || [];
+    if (props.currentNode.data.code) {
+      handleDatasource(
+        props.currentNode.data.taskParams?.readerDatasource || ""
+      );
+    }
+  } else {
+    off();
+  }
+});
+</script>
+<style scoped lang="less">
+.blue-text {
+  color: #2666fb;
+}
+</style>

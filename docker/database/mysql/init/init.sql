4b1008611c2c4ccdfb6dbd32b8407eb2a842b51e
FXB <1072651764@qq.com>
2026-01-15 11:03:08 +0800

    feat(init): 版本脚本更改

--- docker/database/mysql/init/init.sql
+++ docker/database/mysql/init/init.sql
@@ -16544,4 +16544,21 @@ INSERT INTO `system_user_role` VALUES (924, 12);
 INSERT INTO `system_user_role` VALUES (924, 13);
 INSERT INTO `system_user_role` VALUES (924, 14);
 
+
+INSERT INTO `system_dict_data` VALUES(413,5,'SQL Server2008','SQL_Server2008','datasource_type',null,'info','N','0','admin','2025-11-21 19:40:47','admin','2025-11-21 19:41:13',null);
+
+
+
+--清洗规则
+UPDATE `att_clean_rule` SET valid_flag='1' WHERE code='022';
+UPDATE  `att_clean_rule` SET valid_flag='1' WHERE code='009';
+UPDATE  `att_clean_rule` SET valid_flag='1' WHERE code='007';
+
+UPDATE `att_clean_rule`  SET strategy_key='VALUE_REPLACE' WHERE id=2;
+UPDATE `att_clean_rule`  SET strategy_key='PHONE_FORMAT_STD' WHERE id=6;
+UPDATE `att_clean_rule`  SET strategy_key='DATE_FORMAT_STD' WHERE id=7;
+UPDATE `att_clean_rule`  SET strategy_key='DATE_FILL_NULL' WHERE id=17;
+UPDATE `att_clean_rule`  SET strategy_key='PK_DEDUP_FIRST' WHERE id=25;
+
+
 SET FOREIGN_KEY_CHECKS = 1;

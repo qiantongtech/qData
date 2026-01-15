6889cac56cff244f43b3fe24d59cd7a80264ba24
caoyun <1942152752@qq.com>
2026-01-15 15:50:28 +0800

    fix(dict): 修正SQL Server 2008字典数据标签格式

--- sql/mysql/initialization/latest初始化.sql
+++ sql/mysql/initialization/latest初始化.sql
@@ -16576,7 +16576,7 @@ INSERT INTO `system_user_role` VALUES (924, 12);
 INSERT INTO `system_user_role` VALUES (924, 13);
 INSERT INTO `system_user_role` VALUES (924, 14);
 
-INSERT INTO `system_dict_data` VALUES(413,5,'SQL Server2008','SQL_Server2008','datasource_type',null,'info','N','0','admin','2025-11-21 19:40:47','admin','2025-11-21 19:41:13',null);
+INSERT INTO `system_dict_data` VALUES(413,5,'SQL Server 2008','SQL_Server2008','datasource_type',null,'info','N','0','admin','2025-11-21 19:40:47','admin','2025-11-21 19:41:13',null);
 
 
 

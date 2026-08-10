067692b644ae6f616b64cfe3a75f75222fcd634a
zhangtao <1049774729@qq.com>
2026-08-10 13:32:32 +0800

    feat(dp):add  standard code

---
+++ qdata-ui/src/views/dp/dataCodeTable/detail/column/index.vue
@@ -0,0 +1,167 @@
+<!--
+  Copyright © 2025-present Jiangsu Qiantong Technology Co., Ltd.
+
+  This file is part of qData Data Middle Platform (Open Source Edition).
+
+  qData is licensed under Apache License 2.0 with additional qData terms.
+  You may use qData for commercial purposes, but you may not remove, hide,
+  modify, or replace the qData logo, copyright notices, license notices,
+  or attribution information without a separate commercial license.
+
+  White-label use, OEM distribution, rebranding, or presenting qData as
+  another product requires separate commercial authorization from
+  Jiangsu Qiantong Technology Co., Ltd.
+
+  Business License: https://community.qdata.tech/business/policy.html
+  See the LICENSE file in the project root for full license information.
+-->
+
+<template>
+  <div class="app-container">
+    <div class="pagecont-top" style="padding-bottom: 15px">
+      <div class="infotop">
+        <div class="infotop-title mb15">
+          {{ form.name || "-" }}
+        </div>
+        <el-row :gutter="2">
+          <el-col :span="8">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">{{ td('common.texts.number') }}</div>
+              <div class="infotop-row-value">
+                {{ form.id || "-" }}
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">{{ td('dp.dataElem.nameEn') }}</div>
+              <div class="infotop-row-value">
+                {{ form.engName || "-" }}
+              </div>
+            </div>
+          </el-col>
+          <!-- <el-col :span="8">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">Chinese name</div>
+              <div class="infotop-row-value">
+                {{ form.name || "-" }}
+              </div>
+            </div>
+          </el-col> -->
+          <el-col :span="24" style="margin: 2px 0;">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">{{ td('common.texts.description') }}</div>
+              <div class="infotop-row-value">
+                {{ form.description || "-" }}
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">{{ td('dp.dataElem.catCode') }}</div>
+              <div class="infotop-row-value">
+                {{ form.catName || "-" }}
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">{{ td('dp.dataElem.columnType') }}</div>
+              <div class="infotop-row-value">
+                <dict-tag :options="column_type" :value="form.columnType" />
+              </div>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="infotop-row border-top">
+              <div class="infotop-row-lable">{{ td('common.texts.status') }}</div>
+              <div class="infotop-row-value">
+                <dict-tag :options="sys_disable" :value="form.status" />
+              </div>
+            </div>
+          </el-col>
+
+        </el-row>
+      </div>
+    </div>
+    <!-- Tab section -->
+    <div class="pagecont-bottom">
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane :label="td('dp.dataElem.detail.cleanRule')" name="1" lazy>
+          <cleanRule :dataElemId="dataElemId" dataType="2" />
+        </el-tab-pane>
+        <el-tab-pane :label="td('dp.dataElem.detail.auditRule')" name="2" lazy>
+          <auditRule :dataElemId="dataElemId" dataType="1" />
+        </el-tab-pane>
+        <el-tab-pane :label="td('dp.dataElem.detail.relationInfo')" name="3" lazy>
+          <asset />
+        </el-tab-pane>
+        <el-tab-pane :label="td('dp.dataElem.detail.detailInfo')" name="5" lazy>
+          <info :daDiscoveryTaskDetail="form" />
+
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script setup name="dataElemDetailDialog">
+import useDefaultLang from "@/composables/useDefaultLang"
+import { onMounted } from "vue";
+
+const { proxy } = getCurrentInstance();
+
+import { getDpDataElem } from "@/api/dp/dataElem/dataElem";
+
+import cleanRule from "@/views/dp/dataCodeTable/detail/column/cleanRule";
+import auditRule from "@/views/dp/dataCodeTable/detail/column/auditRule";
+import asset from "@/views/dp/dataCodeTable/detail/components/asset.vue";
+import info from "@/views/dp/dataCodeTable/detail/column/info.vue";
+import { useRoute } from "vue-router";
+
+const { td } = useDefaultLang();
+const { column_type, sys_disable } = proxy.useDict(
+  "column_type",
+  "sys_disable"
+);
+
+const dpDataElemRuleRelList = ref([]);
+
+const data = reactive({
+  form: {},
+  activeName: "1",
+});
+const { form, activeName } = toRefs(data);
+const dataElemId = ref("");
+const route = useRoute();
+dataElemId.value = route.query.id;
+
+/** Detail button operation */
+function getDetail() {
+  const id = dataElemId.value;
+  if (!id) return;
+  getDpDataElem(id).then((response) => {
+    form.value = response.data;
+  });
+}
+
+// Get data when page loads
+onMounted(() => {
+  getDetail();
+});
+
+// Return to list page
+function goBack() {
+  router.go(-1);
+}
+</script>
+
+<style scoped lang="scss">
+.app-container {
+  margin: 15px 15px 0px 15px;
+
+  .pagecont-bottom {
+    min-height: calc(100vh - 345px) !important;
+  }
+}
+</style>
---
+++ qdata-ui/src/views/dp/dataCodeTable/detail/column/info.vue
@@ -0,0 +1,139 @@
+<!--
+  Copyright © 2025-present Jiangsu Qiantong Technology Co., Ltd.
+
+  This file is part of qData Data Middle Platform (Open Source Edition).
+
+  qData is licensed under Apache License 2.0 with additional qData terms.
+  You may use qData for commercial purposes, but you may not remove, hide,
+  modify, or replace the qData logo, copyright notices, license notices,
+  or attribution information without a separate commercial license.
+
+  White-label use, OEM distribution, rebranding, or presenting qData as
+  another product requires separate commercial authorization from
+  Jiangsu Qiantong Technology Co., Ltd.
+
+  Business License: https://community.qdata.tech/business/policy.html
+  See the LICENSE file in the project root for full license information.
+-->
+
+<template>
+  <div class="basicInfo">
+    <el-descriptions title="" :column="2" border>
+      <el-descriptions-item v-for="(item, index) in fileDesc" :key="index" label-class-name="base-label"
+        :span="item.span" class-name="base-content">
+        <template #label>
+          <div class="cell-item">{{ item.label }}</div>
+        </template>
+        <div v-if="item.key == 'status'">
+          <dict-tag :options="da_discovery_task_status" :value="daDiscoveryTaskDetail.status" />
+        </div>
+
+        <div v-else-if="item.key == 'createTime'">
+          {{
+            parseTime(
+              daDiscoveryTaskDetail.createTime,
+              "{y}-{m}-{d} {h}:{i}"
+            ) || "-"
+          }}
+        </div>
+        <div v-else-if="item.key == 'createType'">
+          <dict-tag :options="dp_model_create_type" :value="daDiscoveryTaskDetail.createType" />
+        </div>
+        <div v-else-if="item.key == 'updateTime'">
+          {{
+            parseTime(
+              daDiscoveryTaskDetail.updateTime,
+              "{y}-{m}-{d} {h}:{i}"
+            ) || "-"
+          }}
+        </div>
+        <div v-else>{{ getDescValue(item) }}</div>
+      </el-descriptions-item>
+    </el-descriptions>
+  </div>
+</template>
+<script setup name="BasicInfo">
+import useDefaultLang from "@/composables/useDefaultLang"
+import moment from "moment";
+import { cronToZh } from "@/utils/cronUtils";
+
+const { td } = useDefaultLang();
+const { proxy } = getCurrentInstance();
+const { dp_model_status, dp_model_create_type } = proxy.useDict(
+  "dp_model_status",
+  "dp_model_create_type"
+);
+const props = defineProps({
+  daDiscoveryTaskDetail: {
+    type: Object,
+    default: () => { },
+  },
+});
+const fileDesc = computed(() => {
+  return table.value;
+});
+const table = ref([
+  {
+    key: "personChargeName",
+    label: td('dp.dataElem.personCharge'),
+    value: "",
+  },
+  {
+    key: "contactNumber",
+    label: td('dp.dataElem.contactNumber'),
+    value: "",
+  },
+
+  {
+    key: "createBy",
+    label: td('common.texts.createdBy'),
+    value: "",
+  },
+  {
+    key: "createTime",
+    label: td('common.texts.createdTime'),
+    value: "",
+  },
+  {
+    key: "updateBy",
+    label: td('common.texts.updatedBy'),
+    value: "",
+  },
+  {
+    key: "updateTime",
+    label: td('common.texts.updatedTime'),
+    value: "",
+    type: "time",
+  },
+  {
+    key: "remark",
+    label: td('common.texts.remark'),
+    value: "",
+    span: 24,
+  },
+]);
+const getDescValue = (row) => {
+  let detail = { ...props.daDiscoveryTaskDetail };
+  if (props.daDiscoveryTaskDetail) {
+    if (row.type == "time") {
+      row.value = moment(detail[row.key]).format("YYYY-MM-DD");
+    } else if (row.key == "countSubmitted") {
+      row.countSubmitted = detail[row.countSubmitted];
+      row.countPending = detail[row.countPending];
+      row.countIgnoreFlag = detail[row.countIgnoreFlag];
+    } else {
+      row.value = detail[row.key];
+    }
+  }
+  return row.value !== null && row.value !== undefined && row.value !== "" ? row.value : "-";
+};
+</script>
+<style lang="scss" scoped>
+:deep(.base-label) {
+  width: 200px;
+
+  .cell-item {
+    font-weight: 500;
+  }
+}
+</style>
